b73974910524385b9094cbc153782ac22ab2a790
level6 <jiang857447517@GMAIL.COM>
2020-03-09 19:12:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,21 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#修复核心及添加温度显示
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#替换banner
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+#添加wifi
+mkdir -p files/etc/config
+cp -f ../wireless files/etc/config/
+
+#修改机器名称
+sed -i 's/OpenWrt/RaspberryPi4/g' package/base-files/files/bin/config_generate
+
+# Change timezone
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 git clone https://github.com/Level6me/myfeeds.git package/
 #git i package/luci-app-vssr
