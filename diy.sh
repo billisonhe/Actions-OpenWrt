b7fe20ea7310c1cec12a7dfd87a7cf275872c85e
level6 <jiang857447517@GMAIL.COM>
2020-03-09 12:25:14 +0800

    Initial commit

---
+++ diy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+git clone https://github.com/Level6me/myfeeds.git package/
+#git i package/luci-app-vssr
+./scripts/feeds update -a
+./scripts/feeds install -a
